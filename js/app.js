73f781277e4219f198828e52ff8084a704bfdb97
Benjamin J. Kruger <bekroogle@gmail.com>
2015-03-10 00:52:22 -0500

    Added will vs garret and styling.

--- js/app.js
+++ js/app.js
@@ -23,5 +23,20 @@ $('document').ready( function() {
       });
     console.log(result);
   }); // form.submit
+
+  $('#willvgarret').click( function() {
+    choices = ["William", "Garret"];
+    var randomJs = new RandomJs();
+    var result = randomJs
+      .apikey('4ab06f95-751f-45ef-a136-29d093323fa3')
+      .method('generateIntegers')
+      .params({n:1,min:1,max:choices.length})
+      .post(function(xhr, stream, body) {
+        
+        console.log(choices[body.result.random.data[0]-1]);
+        $('#result').html('And the winner is...' + choices[body.result.random.data[0]-1]);
+      });
+    console.log(result);
+  });
 }); // document.ready
 //$( elem ).prop( "checked" )
